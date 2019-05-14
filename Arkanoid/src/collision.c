c51bffe9a21841259a601d66111d04d26a32409c
FrancoisHaegel <haegelfrancois@yahoo.fr>
2019-05-14 17:31:43 +0200

    [TEST] (collision) : tests sur les collisions latérales

--- Arkanoid/src/collision.c
+++ Arkanoid/src/collision.c
@@ -4,39 +4,52 @@
 void colliding(){
 	 if(ceil(((ball.y)/16)<20))
  	{
- 		int col = (int)((ball.x)/32);
-		int row = (int)(((ball.y)/16));
+ 		int col = (int)((ball.x + ball.rayon)/32);
+		int row = (int)(((ball.y + ball.rayon)/16));
 	  //check if a brick is at this location
-		 if(brick_list[col][row].bc != TRANSPARENT){
-		resolveCollision(&brick_list[col][row]);
+		if(col > -1 && col < 13 && row > -1 && row < 20){
+			if(brick_list[col][row].bc != TRANSPARENT){
+	 			resolveCollision(&brick_list[col][row]);
+	 		}
 		}
 	}
 }
 
 void resolveCollision(BRICK* brick){
-    ball.vy = ball.vy * (-1);
-    brick->bc = TRANSPARENT;
 
-		// double brickCenterX = brick->x + 16;
-		// double brickCenterY = brick->y + 8;
-		//
-		// double ballCenterX = ball.x + ball.rayon;
-		// double ballCenterY = ball.y + ball.rayon;
-		//
-		// //si on touche sur le bas de la brique
-		// if(ballCenterY > brickCenterY + 8){
-		//
+	 	double ballCenterX = ball.x + ball.rayon;
+		double ballCenterY = ball.y + ball.rayon;
+
+		// if(brick->x - ball.rayon <= ballCenterX && ballCenterX <= brick->x + ball.rayon + 32){
+		// 	ball.vy = ball.vy * (-1);
+		//   brick->bc = TRANSPARENT;
+		// }else if(brick->y - ball.rayon <= ballCenterY && ballCenterY <= brick->y + ball.rayon + 16){
+		// 	ball.vx = ball.vx * (-1);
+		//   brick->bc = TRANSPARENT;
 		// }
-		// //si on touche le haut de la brique
-		// else if(ballCenterY < brickCenterY - 8){
-		//
-		// }
-		// //si on touche la gauche de la brique
-		// else if(ballCenterX > brickCenterX + 16){
-		//
-		// }
-		// //si on touche la droite de la brique
-		// else{
-		//
-		// }
+
+
+		
+		double brickCenterX = brick->x + 16;
+		double brickCenterY = brick->y + 8;
+		//si on touche sur le bas de la brique
+		if(ballCenterY > brickCenterY + 8){
+			ball.vy = ball.vy * (-1);
+	    brick->bc = TRANSPARENT;
+		}
+		//si on touche le haut de la brique
+		else if(ballCenterY < brickCenterY - 8){
+			ball.vy = ball.vy * (-1);
+	    brick->bc = TRANSPARENT;
+		}
+		//si on touche la gauche de la brique
+		else if(ballCenterX > brickCenterX + 16){
+			ball.vx = ball.vx * (-1);
+	    brick->bc = TRANSPARENT;
+		}
+		//si on touche la droite de la brique
+		else{
+			ball.vx = ball.vx * (-1);
+	    brick->bc = TRANSPARENT;
+		}
 }
